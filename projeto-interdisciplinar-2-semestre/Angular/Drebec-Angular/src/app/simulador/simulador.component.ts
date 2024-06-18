f12d6e4e984d1979d3fd8aacac6775a31039f48a
luizriato <142940208+luizriato@users.noreply.github.com>
2024-06-18 14:05:43 -0300

    update

--- projeto-interdisciplinar-2-semestre/Angular/Drebec-Angular/src/app/simulador/simulador.component.ts
+++ projeto-interdisciplinar-2-semestre/Angular/Drebec-Angular/src/app/simulador/simulador.component.ts
@@ -1,53 +1,127 @@
-import { Component, OnInit } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 @Component({
   selector: 'app-simulador',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatDialogModule],
   templateUrl: './simulador.component.html',
   styleUrl: './simulador.component.scss'
 })
-export class SimuladorComponent implements OnInit {
-  public solar: boolean = false;
-  public eolica: boolean = false;
-  public biomassa: boolean = false;
 
-  alterarVisibilidade(tamanho: string, tipo: string): void {
-    this.resetFlags();
-    if (tipo === 'casa') {
-      if (tamanho === 'pequena') {
-        this.solar = true;
-      } else if (tamanho === 'media') {
-        this.solar = true;
-        this.biomassa = true;
-      } else if (tamanho === 'grande') {
-        this.solar = true;
-        this.eolica = true;
-        this.biomassa = true;
-      }
-    } else if (tipo === 'apartamento') {
-      if (tamanho === 'media' || tamanho === 'grande') {
-        this.solar = true;
-      }
-    } else if (tipo === 'rural') {
-      if (tamanho === 'pequena') {
-        this.solar = true;
-      } else if (tamanho === 'media') {
-        this.biomassa = true;
-      } else if (tamanho === 'grande') {
-        this.eolica = true;
-      }
-    }
+
+export class SimuladorComponent {
+  constructor(public dialog: MatDialog) { }
+
+  openDialogSolar() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
   }
-ngOnInit(): void {
-    
-}
-  resetFlags(): void {
-    this.solar = false;
-    this.eolica = false;
-    this.biomassa = false;
+
+  openDialogEolica() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog2);
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
+  }
+
+  openDialogBiomassa() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog3);
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
+  }
+
+  openDialogSolarBiomassa() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog4);
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
+  }
+
+  openDialogTodos() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog5);
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
+  }
+
+  openDialogNenhum() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog6);
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+
   }
 }
 
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: 'dialog-content-example-dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatCardModule, MatButtonModule, MatDialogModule],
+})
 
+export class DialogContentExampleDialog { }
+
+@Component({
+  selector: 'dialog-content-example-dialog2',
+  templateUrl: 'dialog-content-example-dialog2.html',
+  standalone: true,
+  imports: [MatDialogModule, MatCardModule, MatButtonModule, MatDialogModule],
+})
+
+export class DialogContentExampleDialog2 { }
+
+@Component({
+  selector: 'dialog-content-example-dialog3',
+  templateUrl: 'dialog-content-example-dialog3.html',
+  standalone: true,
+  imports: [MatDialogModule, MatCardModule, MatButtonModule, MatDialogModule],
+})
+export class DialogContentExampleDialog3 { }
+
+@Component({
+  selector: 'dialog-content-example-dialog4',
+  templateUrl: 'dialog-content-example-dialog4.html',
+  standalone: true,
+  imports: [MatDialogModule, MatCardModule, MatButtonModule, MatDialogModule],
+})
+export class DialogContentExampleDialog4 { }
+
+
+@Component({
+  selector: 'dialog-content-example-dialog5',
+  templateUrl: 'dialog-content-example-dialog5.html',
+  standalone: true,
+  imports: [MatDialogModule, MatCardModule, MatButtonModule, MatDialogModule],
+})
+export class DialogContentExampleDialog5 { }
+
+@Component({
+  selector: 'dialog-content-example-dialog6',
+  templateUrl: 'dialog-content-example-dialog6.html',
+  standalone: true,
+  imports: [MatDialogModule, MatCardModule, MatButtonModule, MatDialogModule],
+})
+export class DialogContentExampleDialog6 { }
